4770e169e8b7c8050014d7472f6460a42d3a536f
Aleksandra <shurochka311@gmail.com>
2024-10-16 14:57:34 +0300

    3 tasks from 4 modules

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let statusCell = row.cells[3];
+    if (statusCell.hasAttribute('data-available')) {
+      if (statusCell.getAttribute('data-available') === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.hidden = true;
+    }
+    let genderCell = row.cells[2];
+    if (genderCell.textContent === 'm') {
+      row.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      row.classList.add('female');
+    }
+    let ageCell = row.cells[1];
+    if (parseInt(ageCell.textContent) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
